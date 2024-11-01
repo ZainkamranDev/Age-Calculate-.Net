0c961ae7f15813d84ea66df3f0a667f87a539889
ZainkamranDev <zainkamran006@gmail.com>
2024-11-01 10:26:49 +0500

    Add files via upload

---
+++ Calculate Age/Calculate Age/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 age1 = new Class1();
+            Console.WriteLine(age1.calculate_age());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Calculate Age/Calculate Age/Properties/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace result
+{
+    internal class Class1
+    {
+
+        private static int current_year = 2024;
+        private static int birth_year = 2006;
+
+
+        public int calculate_age()
+        {
+
+
+
+            int age = current_year - birth_year;
+            return age;
+        }
+    }
+}
